681594fd0ce52be8e188e181ec855e7f37d143dc
naecoo <ocean7hy@gmail.com>
2022-10-06 23:08:40 +0800

    add pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/emoji-mix/',
   resolve: {
     alias: {
       '@': resolve(__dirname, './src')
@@ -11,6 +13,20 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    VueSetupExtend()
+    VueSetupExtend(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      scope: '/emoji-mix/',
+      manifest: {
+        name: 'emoji-mix',
+        short_name: 'emoji-mix',
+        description: 'Combined two emojis into one',
+        start_url: '/emoji-mix/',
+        display: 'standalone',
+        background_color: '#f78e48',
+        theme_color: '#f78e48',
+        prefer_related_applications: true,
+      },
+    })
   ]
 })
